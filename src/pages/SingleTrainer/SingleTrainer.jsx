0ea93cceaf7b00dd64e5f52f8b1532c4171cf803
Md Najrul Islam <mdnajrulislam.4738@gmail.com>
2023-12-04 19:25:31 +0600

    update trainer's single page

--- src/pages/SingleTrainer/SingleTrainer.jsx
+++ src/pages/SingleTrainer/SingleTrainer.jsx
@@ -1,5 +1,53 @@
+import { useLoaderData } from "react-router-dom";
+import SectionHeader from "../../Components/SectionHeader/SectionHeader";
+import Container from "../Shared/Container/Container";
+
 const SingleTrainer = () => {
-  return <div></div>;
+  const trainerInfo = useLoaderData();
+  console.log(trainerInfo);
+  const {
+    name,
+    email,
+    age,
+    image,
+    experience,
+    skills,
+    available_time_in_a_date,
+    social_links,
+    train_category,
+    price,
+  } = trainerInfo;
+  return (
+    <div className="my-10">
+      <Container>
+        <SectionHeader title={"Trainer Information"}></SectionHeader>
+        <div className="grid gap-10 md:grid-cols-2 ">
+          <div>
+            <img src={image} alt="" className="w-full" />
+            <div></div>
+          </div>
+          <div>
+            <h3 className="md:text-xl lg:text-2xl">
+              Trainer's Name: <span className="text-slate-700">{name}</span>
+            </h3>
+            <div>
+              <h3>Available slots. </h3>
+              <ul>
+                {available_time_in_a_date?.map((slot, index) => (
+                  <li
+                    key={index}
+                    className="list-none border py-2 cursor-pointer mt-5 w-fit"
+                  >
+                    Slot-{index + 1}: {slot}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default SingleTrainer;
